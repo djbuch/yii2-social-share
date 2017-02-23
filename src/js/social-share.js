6e34a88e242e22735e258d399ccf7e2a755565d7
Code Utopia <buchdj@gmail.com>
2017-02-23 10:34:10 +0100

    Update social-share.js

--- src/js/social-share.js
+++ src/js/social-share.js
@@ -1,3 +1,23 @@
 function sharePopup(url){
-    window.open(url , '123456789' , 'height=368,width=585,resizeable=yes');
+    if (url.indexOf("mailto:") > -1) {
+        window.location.href = url;
+    } else {
+        var w = 585;
+        var h = 368;
+        // Fixes dual-screen position                         Most browsers      Firefox
+        var dualScreenLeft = window.screenLeft != undefined ? window.screenLeft : screen.left;
+        var dualScreenTop = window.screenTop != undefined ? window.screenTop : screen.top;
+
+        var width = window.innerWidth ? window.innerWidth : document.documentElement.clientWidth ? document.documentElement.clientWidth : screen.width;
+        var height = window.innerHeight ? window.innerHeight : document.documentElement.clientHeight ? document.documentElement.clientHeight : screen.height;
+
+        var left = ((width / 2) - (w / 2)) + dualScreenLeft;
+        var top = ((height / 2) - (h / 2)) + dualScreenTop;
+        var newWindow = window.open(url , '123456789' , ',resizeable=yes, width=' + w + ', height=' + h + ', top=' + top + ', left=' + left);
+
+        // Puts focus on the newWindow
+        if (window.focus) {
+            newWindow.focus();
+        }
+    }
 }
