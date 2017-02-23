ca50a423fdef1476ee0dcfd8dc78105c5dca53c3
Code Utopia <buchdj@gmail.com>
2017-02-23 10:34:32 +0100

    Update Share.php

--- src/Share.php
+++ src/Share.php
@@ -7,10 +7,14 @@ use yii\helpers\Html;
 
 class Share extends Widget {
 
+    public $list;
+
     public function run() {
         echo Html::beginTag($this->tag , $this->htmlOptions);
-        foreach ($this->networks as $key => $val) {
-            echo $this->parseTemplate($key);
+        if ($this->list === null)
+            $this->list = array_keys($this->networks);
+        foreach ($this->list as $val) {
+            echo $this->parseTemplate($val);
         }
         echo Html::endTag($this->tag);
     }
